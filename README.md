30727f105c828d53bdbe09e1a6e9af7ad9753c1d
Elias-Garcia <51672630+jibsamGarcia@users.noreply.github.com>
2020-01-27 17:02:00 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# eliasgarcia
+My portfolio built using Materialize framework and Animate.css
+
+
+## Content
+- Home Page
+- About Me
+- Skills
+- Recent Project
+- Discover More
+- Contact
+___
+
+## Website & Blog
+- [E'G Portfolio](https://github.com/jibsamGarcia/)
+
+___
+
+## Authors
+[Elias.García](https://github.com/jibsamGarcia)
+eliasgarcia
---
+++ js/index.js
@@ -0,0 +1,212 @@
+(function () {
+  const projects = [
+    {
+      title: 'Hackter méxico',
+      image: './img/projects/hackterfull.png',
+      imageAlt: 'Chrome Extension Project Cover Photo',
+      description: 'Hackter es una escuela en la que aprenderás a programar y a crear productos de software innovadores',
+      badgeCaption: 'Chrome Extension',
+      links: {
+        github: 'https://github.com/jibsamGarcia?tab=repositories',
+        chrome: 'https://www.instagram.com/garciajibsam/'
+      },
+      accomplishments: [
+        'Javascript',
+        'Built with Bootstrap 4.3',
+        'Using Javascript ES6'
+      ]
+    },
+    {
+      title: 'Instituto OWL',
+      image: './img/projects/instituto.png',
+      imageAlt: 'Favorite OWL Photo',
+      description: 'Instituto OWL Comunidad Educativa.',
+      badgeCaption: 'Node.js & Express',
+      links: {
+        github: 'https://github.com/jibsamGarcia?tab=repositories',
+        chrome: 'https://www.instagram.com/garciajibsam/'
+      },
+      accomplishments: [
+        'Express Handlebars & Bootstrap',
+        'MongoDB & Mongoose',
+        'Followed MVC Framework',
+        'Materialize,css3,Javascript'
+      ]
+    },
+    {
+      title: 'Concord futbol',
+      image: './img/projects/concord.png',
+      imageAlt: 'Favorite concord photo',
+      description: 'Ecommerce store Sportpro',
+      badgeCaption: 'Node.js & Express',
+      links: {
+        github: 'https://github.com/jibsamGarcia?tab=repositories',
+        instagram: 'https://www.instagram.com/garciajibsam/'
+      },
+      accomplishments: [
+        'Express Handlebars & Bootstrap',
+        'MongoDB & Mongoose',
+        'WordPress',
+        'Front-end & Back-end validation'
+      ]
+    },
+    {
+      title: 'Proyecto Android',
+      image: './img/projects/cap2.png',
+      imageAlt: 'Code Photo',
+      description: 'Es una application commerce store codificada en android studio y swift para IOS',
+      badgeCaption: 'Node.js & Express',
+      links: {
+        github: 'https://github.com/jibsamGarcia?tab=repositories',
+        instagram: 'https://www.instagram.com/garciajibsam/'
+      },
+      accomplishments: [
+        'Android studio',
+        'Javascript',
+        'Passport Authentication',
+        'Front-end & Back-end validation',
+        'Apis',
+        'Swift',
+      ]
+    }
+  ]
+
+
+
+  const nav = document.querySelector('nav')
+  const navHeight = nav.offsetHeight
+  const skillOffsetHeight = document.getElementById('skill').offsetTop
+  const projectOffsetHeight = document.getElementById('project').offsetTop
+  const actionBtn = document.querySelector('.fixed-action-btn a:first-of-type')
+  let skillsAnimated = false
+  let aboutCardAnimated = false
+  let projectPlaced = false
+  let learnMoreAnimated = false
+
+  // Handle animation end
+  function handleAnimationEnd(element, animations) {
+    element.classList.remove(...animations)
+  }
+
+  // Handle navbar animation
+  function animateNav() {
+    if (window.pageYOffset > navHeight) { return nav.classList.add('blue-grey', 'lighten-3', 'shadow') }
+    nav.classList.remove('blue-grey', 'lighten-3', 'shadow')
+  }
+
+  // Handle floating action button
+  function showFloatingActionButton() {
+    if (window.pageYOffset > navHeight) { return actionBtn.classList.remove('scale-out') }
+    actionBtn.classList.add('scale-out')
+  }
+
+  // Handle about cards animation
+  function animateAboutCards() {
+    if (window.pageYOffset <= navHeight) { return }
+    const aboutCards = document.querySelectorAll('.section-about .card')
+    aboutCardAnimated = true
+    aboutCards.forEach(card => card.classList.add('jackInTheBox', 'slow'))
+  }
+
+  // Handle skill section animation
+  function animateSkills() {
+    if (window.pageYOffset + window.innerHeight <= skillOffsetHeight) { return }
+    const firstSkillSection = document.getElementById('front-end-carousel-item')
+    const animations = ['animated', 'slideInRight']
+    skillsAnimated = true
+    firstSkillSection.classList.add(...animations)
+    firstSkillSection.addEventListener('animationend', () => handleAnimationEnd(event.target, animations))
+  }
+
+  // Handle learn more section animation
+  function animateLearnMore() {
+    const learnMoreOffsetHeight = document.getElementById('more-info').offsetTop
+    if (window.pageYOffset + window.innerHeight <= learnMoreOffsetHeight) { return }
+    const buttons = document.querySelectorAll('.learn-more-link a')
+    const animations = ['animated', 'bounceIn', 'slow']
+    learnMoreAnimated = true
+    buttons.forEach(button => button.classList.add(...animations))
+  }
+
+  // Generate icons of link
+  function getIconLinks(links) {
+    let icons = ``
+    if (links.github) {
+      icons += `<a href=${links.github} class="right btn-floating btn-small waves-effect waves-light teal" target="_blank"><i class="fab fa-github"></i></a>`
+    }
+    if (links.medium) {
+      icons += `<a href=${links.medium} class="right btn-floating btn-small waves-effect waves-light teal" target="_blank"><i
+                  class="fab fa-medium-m"></i></a>`
+    }
+    if (links.chrome) {
+      icons += `<a href=${links.chrome}
+                class="right btn-floating btn-small waves-effect waves-light teal" target="_blank"><i class="fab fa-chrome"></i></a>`
+    }
+    if (links.heroku) {
+      icons += `<a href=${links.heroku}
+                class="right btn-floating btn-small waves-effect waves-light teal" target="_blank"><i class="fas fa-home"></i></a>`
+    }
+    return icons
+  }
+
+  function getAccomplishments(accomplishments) {
+    return accomplishments.map(point => `<li><i class="fas fa-caret-right"></i>${point}</li>`).join('')
+  }
+
+  // Place all projects into project section
+  function placeProjects() {
+    // place projects when scroll to project section
+    if (window.pageYOffset + window.innerHeight <= projectOffsetHeight) { return }
+    // get elements
+    const projectSection = document.querySelector('.section-project .row')
+    // switch status to placed
+    projectPlaced = true
+    // generate html for each project
+    projects.forEach(project => {
+      // Get all icon links
+      const icons = getIconLinks(project.links)
+      // Gather all accomplishments
+      const accomplishments = getAccomplishments(project.accomplishments)
+
+      projectSection.innerHTML += `
+      <div class="col s12 m6 animated flipInX">
+        <div class="card sticky-action hoverable">
+          <div class="card-image waves-effect waves-block waves-light">
+            <img class="responsive-img activator" src=${project.image}
+              alt="${project.image} Project Cover Photo">
+            <div class="overlay"></div>
+            <span class="card-title activator">${project.title}</span>
+          </div>
+          <div class="card-action">
+            <p class="activator truncate"><span class="new badge right activator"
+              data-badge-caption="${project.badgeCaption}"></span>${project.title}</p>
+          </div>
+          <div class="card-reveal">
+            <div class="overlay"></div>
+            <span class="card-title white-text">Accomplishments<i class="material-icons right">close</i></span>
+            <ul class="white-text">
+              ${accomplishments}
+            </ul>
+            <div id="card-reveal-icons">
+              ${icons}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col m5 hide-on-med-and-down offset-m1 valign-wrapper">
+        <h5 class="blue-grey-text text-darken-1">${project.title}</h5>
+        <span class="blue-grey-text text-lighten-1">${project.description}</span>
+      </div>
+    `
+    })
+  }
+
+  window.addEventListener('scroll', () => {
+    animateNav()
+    showFloatingActionButton()
+    if (!aboutCardAnimated) { animateAboutCards() }
+    if (!skillsAnimated) { animateSkills() }
+    if (!projectPlaced) { placeProjects() }
+    if (!learnMoreAnimated && projectPlaced) { animateLearnMore() }
+  })
+})()
